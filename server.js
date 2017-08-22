68a64a92fe555aa9ad170d6c1490fa41c917752c
soundar-git <soundar.amazon@gmail.com>
2017-08-22 16:37:41 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-one',function(req,res) {
+    res.send('Article-one file will be served here')
+});
+
+app.get('/Article-two',function(req,res) {
+    res.send('Article-two file will be served here')
+});
+
+app.get('/Article-three',function(req,res) {
+    res.send('Article-three file will be served here')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
