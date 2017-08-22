6823ebe7a62132453a4df819bd1c50540394ea21
soundar-git <soundar.amazon@gmail.com>
2017-08-22 18:11:52 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,110 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one': {
+        title: 'Article One | Soundar',
+        heading: 'Article One',
+        date: 'Sep 21, 2017',
+        content: `
+             <p>
+                   This is the content for my first article - 1a
+               </p>
+               <p>
+                   This is the content for my first article - 1b
+               </p>
+               <p>
+                   This is the content for my first article - 1c
+               </p>`        
+    },
+    'article-two': {
+        title: 'Article Two | Soundar',
+        heading: 'Article Two',
+        date: 'Sep 22, 2017',
+        content: `
+                   <p>
+                   This is the content for my second article - 2a
+               </p>
+               <p>
+                   This is the content for my second article - 2b
+               </p>
+               <p>
+                   This is the content for my second article - 2c
+               </p>`    
+    },
+    'article-three': {
+        title: 'Article Three | Soundar',
+        heading: 'Article Three',
+        date: 'Sep 23, 2017',
+        content: `
+                       <p>
+                   This is the content for my 3rd article - 3a
+               </p>
+               <p>
+                   This is the content for my 3rd article - 3b
+               </p>
+               <p>
+                   This is the content for my 3rd article - 3c
+               </p>`
+        
+    }
+}
+
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+           <h3>
+               ${heading}
+           </h3> 
+           <div>
+               ${date}
+           </div>
+           <div>
+                ${content}
+           </div>
+       </div>
+    </body>
+</html>`
+}
+
+app.get('/:articleName',function(req,res) {
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-one',function(req,res) {
-    res.sendFile(path.join(__dirname,'ui','article-one.html'))
-});
+//app.get('/Article-one',function(req,res) {
+    //res.sendFile(path.join(__dirname,'ui','article-one.html'))
+//});
 
-app.get('/Article-two',function(req,res) {
-    res.sendFile(path.join(__dirname,'ui','article-two.html'))
-});
+//app.get('/Article-two',function(req,res) {
+    //res.sendFile(path.join(__dirname,'ui','article-two.html'))
+//});
 
-app.get('/Article-three',function(req,res) {
-    res.sendFile(path.join(__dirname,'ui','article-three.html'))
-});
+//app.get('/Article-three',function(req,res) {
+    //res.sendFile(path.join(__dirname,'ui','article-three.html'))
+//});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
