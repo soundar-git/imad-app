08d7c73c9691a0f73d7a74d7ad5a3a4a84b52d14
soundar-git <soundar.amazon@gmail.com>
2017-08-23 00:24:21 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'soundararajanananthu',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password:DB.PASSWORD
+    password: process.env.DB.PASSWORD
 };
 
 var app = express();
